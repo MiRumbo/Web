74dc449b26e186da86f1ae792f572249ac1c98ff
ValessioBrito <contato@valessiobrito.com.br>
2014-12-01 16:42:35 -0200

    Set Google Web Fonts to Enviroment

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,9 @@
 
 	@yield('title')
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Dosis:400,700|Cantarell:400,700,400italic|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
 	{{ HTML::style('bootstrap/css/bootstrap.min.css', array('media' => 'screen')) }}
+	{{ HTML::style('style.css', array('media' => 'screen')) }}
 
    <body>
 
